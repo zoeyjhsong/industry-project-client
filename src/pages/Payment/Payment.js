3aac496653d75a538e138133f3c606a9d65cd500
Harsh Shah <harshvshah.22@gmail.com>
2024-02-10 13:35:47 -0500

    dummy payment setup for frontend

---
+++ src/pages/Payment/Payment.js
@@ -0,0 +1,76 @@
+import React, { useState } from 'react';
+
+const Payment = () => {
+  const [formData, setFormData] = useState({
+    cardNumber: '',
+    expMonth: '',
+    expYear: '',
+    cvc: '',
+    name: '',
+    zip: ''
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Payment submitted!');
+    // Reset form after submission
+    setFormData({
+      cardNumber: '',
+      expMonth: '',
+      expYear: '',
+      cvc: '',
+      name: '',
+      zip: ''
+    });
+  }
+
+  const { cardNumber, expMonth, expYear, cvc, name, zip } = formData;
+
+  return (
+    <div>
+      <h2>Payment Details</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Card Number:
+          <input type="text" name="cardNumber" value={cardNumber} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Expiry Month:
+          <input type="text" name="expMonth" value={expMonth} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Expiry Year:
+          <input type="text" name="expYear" value={expYear} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          CVC:
+          <input type="text" name="cvc" value={cvc} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          Name:
+          <input type="text" name="name" value={name} onChange={handleChange} />
+        </label>
+        <br />
+        <label>
+          ZIP:
+          <input type="text" name="zip" value={zip} onChange={handleChange} />
+        </label>
+        <br />
+        <button type="submit">Submit Payment</button>
+      </form>
+    </div>
+  );
+}
+
+export default Payment;
